63ff726dd0046f857b989e8665472b523487980d
Darcson <dcn.albuquerque@gmail.com>
2018-07-07 22:51:55 +1200

    Implementing all principal verbs

---
+++ RestWithAspNETUdemy 02 - Using Different Verbs/RestWithAspNETUdemy/Model/Person.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RestWithAspNETUdemy.Model
+{
+    public class Person
+    {
+        public long Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Address { get; set; }
+        public GenderEnum Gender { get; set; }
+    }
+
+    public enum GenderEnum
+    {
+        Masculino,
+        Feminino
+    }
+}
---
+++ RestWithAspNETUdemy 02 - Using Different Verbs/Utilities/Extension/NumericExtensions.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Utilities
+{
+    public static class NumericExtensions
+    {
+        #region commom
+        public static bool IsNumeric(this string strValue)
+        {
+            var temp = 0.0;
+            return !string.IsNullOrEmpty(strValue.Trim()) && double.TryParse(strValue, System.Globalization.NumberStyles.Any, System.Globalization.NumberFormatInfo.InvariantInfo, out temp);
+        }
+        #endregion
+
+        #region decimal
+        public static decimal ConvertToDecimal(this string strValue)
+        {
+            var decimalValue = 0m;
+            if (decimal.TryParse(strValue, out decimalValue))
+            {
+                return decimalValue;
+            }
+
+            throw new ApplicationException($"ConvertToDecimal: Error converting {strValue} to decimal");
+        }
+        #endregion
+    }
+}
